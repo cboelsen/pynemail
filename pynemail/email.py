ce6bc401dc587cb933d10b75b71aef8fc86b1501
Christian Boelsen <christianboelsen+github@gmail.com>
2018-05-04 15:53:34 +0100

    Remove premature optimisation with email flags.

--- pynemail/email.py
+++ pynemail/email.py
@@ -21,17 +21,11 @@ class Email:
     parser = BytesParser(policy=default_policy)
 
     def __init__(self):
-        self._date = None
         self._headers = None
         self._message = None
         self._body = None
         self._from = None
         self._flags = None
-        self._unread = None
-        self._important = None
-        self._replied = None
-        self._deleted = None
-        self._draft = None
 
     def _get_flags(self) -> EmailMessage:
         raise NotImplementedError('Email._get_flags()')
@@ -43,29 +37,19 @@ class Email:
         raise NotImplementedError('Email._get_message()')
 
     def unread(self) -> bool:
-        if self._unread is None:
-            self._unread = EmailFlag.SEEN not in self.flags()
-        return self._unread
+        return EmailFlag.SEEN not in self.flags()
 
     def important(self) -> bool:
-        if self._important is None:
-            self._important = EmailFlag.FLAGGED in self.flags()
-        return self._important
+        return EmailFlag.FLAGGED in self.flags()
 
     def replied(self) -> bool:
-        if self._replied is None:
-            self._replied = EmailFlag.ANSWERED in self.flags()
-        return self._replied
+        return EmailFlag.ANSWERED in self.flags()
 
     def deleted(self) -> bool:
-        if self._deleted is None:
-            self._deleted = EmailFlag.DELETED in self.flags()
-        return self._deleted
+        return EmailFlag.DELETED in self.flags()
 
     def draft(self) -> bool:
-        if self._draft is None:
-            self._draft = EmailFlag.DRAFT in self.flags()
-        return self._draft
+        return EmailFlag.DRAFT in self.flags()
 
     def headers(self) -> EmailMessage:
         if self._headers is None:
@@ -128,7 +112,6 @@ class Email:
     def clear(self) -> None:
         """Clear all cached state to force the lazy initialisers to reload."""
         self.clear_flags()
-        self._date = None
         self._headers = None
         self._message = None
         self._body = None
@@ -137,8 +120,3 @@ class Email:
     def clear_flags(self) -> None:
         """Clear all cached flags to force the lazy initialisers to reload."""
         self._flags = None
-        self._unread = None
-        self._important = None
-        self._replied = None
-        self._deleted = None
-        self._draft = None
